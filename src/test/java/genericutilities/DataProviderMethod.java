ebc087f093bae8473c7d65b405f8a425a50c41ad
Syednaseer93 <syednaseer1993@gmail.com>
2023-03-23 16:23:40 +0530

    updated

--- src/test/java/genericutilities/DataProviderMethod.java
+++ src/test/java/genericutilities/DataProviderMethod.java
@@ -2,19 +2,23 @@ package genericutilities;
 
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 public class DataProviderMethod {
-
-	public void getMultipleData(String path, int row, int column) {
+	
+@DataProvider
+	public Iterator<Object[]> getMultipleData(/*String path, String sheet, int row, int column*/) {
 		ArrayList<Object[]> dataList = new ArrayList<Object[]>();
 		try {
-			Workbook wb=WorkbookFactory.create(new FileInputStream(path));
+			Workbook wb=WorkbookFactory.create(new FileInputStream("./data/DP1.xlsx"));
 
-			int rowCount=wb.getSheet("").getLastRowNum();
-			int colCount=wb.getSheet(path).getRow(row).getLastCellNum();
+			int rowCount=wb.getSheet("sheet1").getLastRowNum();
+			int colCount=wb.getSheet("sheet1").getRow(1).getLastCellNum();
 
 			for(int i=0;i<=rowCount;i++) 
 			{
@@ -22,7 +26,7 @@ public class DataProviderMethod {
 
 				for(int j=0;j<colCount;j++) {
 					try {
-						String v=wb.getSheet(path).getRow(row).getCell(column).getStringCellValue();
+						String v=wb.getSheet("sheet1").getRow(i).getCell(j).getStringCellValue();
 						dataRow[j]=v;
 					}
 					catch(Exception e) {
@@ -35,6 +39,12 @@ public class DataProviderMethod {
 		catch(Exception e) {
 			e.printStackTrace();
 		}
-
+			Iterator<Object[]> data = dataList.iterator();
+			return data;
+	}
+	@Test(dataProvider = "getMultipleData")
+	public void useData(String a1,String b1, String b3) {
+		System.out.println(a1+b1+b3);
+		
 	}
 }
